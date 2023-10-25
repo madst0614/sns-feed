6852240d019ce0fa8c752d7ba02db3b6b805433a
HyunsooZo <bzhs1992@icloud.com>
2023-10-25 19:21:39 +0900

    feat: User 관련 Exception 코드 추가
    USER_NOT_FOUND - 사용자를 찾을수 없을 때 예외

--- src/main/java/wanted/n/exception/ErrorCode.java
+++ src/main/java/wanted/n/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     //GlobalException
-    UNDEFINED_EXCEPTION(HttpStatus.BAD_REQUEST, "알 수 없는 오류입니다.");
+    UNDEFINED_EXCEPTION(HttpStatus.BAD_REQUEST, "알 수 없는 오류입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
