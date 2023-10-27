ba0e024042d8ad3e366e307c103c671dbf7d9a3d
HyunsooZo <bzhs1992@icloud.com>
2023-10-27 18:36:40 +0900

    feat: 회원 인증 관련 예외 추가

--- src/main/java/wanted/n/exception/ErrorCode.java
+++ src/main/java/wanted/n/exception/ErrorCode.java
@@ -19,7 +19,11 @@ public enum ErrorCode {
     INVALID_PASSWORD_AT_LEAST_2_TYPES(HttpStatus.BAD_REQUEST, "숫자, 문자, 특수문자 중 2가지 이상을 포함해야 합니다."),
     INVALID_PASSWORD_SAME_CHARACTERS(HttpStatus.BAD_REQUEST, "동일한 문자가 3개 이상 연속되면 사용할 수 없습니다"),
     INVALID_PASSWORD_CONSECUTIVE_CHARACTERS(HttpStatus.BAD_REQUEST, "연속된 문자가 3개 이상 포함되면 사용할 수 없습니다"),
-    INVALID_PASSWORD_USUAL_PASSWORD(HttpStatus.BAD_REQUEST, "통상적으로 자주 사용되는 비밀번호는 사용할 수 없습니다.");
+    INVALID_PASSWORD_USUAL_PASSWORD(HttpStatus.BAD_REQUEST, "통상적으로 자주 사용되는 비밀번호는 사용할 수 없습니다."),
+    ALREADY_VERIFIED_USER(HttpStatus.BAD_REQUEST, "이미 인증된 사용자입니다."),
+    PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    INVALID_OTP(HttpStatus.BAD_REQUEST, "인증코드가 일치하지 않습니다."),
+    OTP_EXPIRED(HttpStatus.BAD_REQUEST, "요청되지 않은 이메일이거나 인증기간이 만료된 인증번호입니다. 인증번호를 다시 요청해주세요.");
 
     private final HttpStatus status;
     private final String message;
