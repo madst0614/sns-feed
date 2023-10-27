7cc43c21d5018e86edecf686d61416af5791f17c
12hyeon <hyeonjeong1212@naver.com>
2023-10-27 14:23:21 +0900

    feat: 3시간 초과된 Log 제거

--- src/main/java/wanted/n/service/RedisService.java
+++ src/main/java/wanted/n/service/RedisService.java
@@ -7,6 +7,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import wanted.n.dto.LogDTO;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 @RequiredArgsConstructor
 public class RedisService {
@@ -15,15 +17,21 @@ public class RedisService {
     private final ObjectMapper objectMapper;
 
     private final static String KEY_TAG = "tag:";
+    private final static Integer TIMES = 3 * 60 * 60 * 1000;
 
     /**
-     * 객체를 JSON 형식으로 변환시켜 redis의 sorted set에 저장합니다.
+     * 객체를 JSON 형식으로 변환시켜 3시간 동안 redis의 sorted set에 저장합니다.
      *
      * @throws JsonProcessingException JSON 변환 예외 발생 처리
      */
     public void saveObjectAsJson(LogDTO log) throws JsonProcessingException {
         String jsonValue = objectMapper.writeValueAsString(log);
-        redisTemplate.opsForZSet().add(KEY_TAG + log.getTag(), jsonValue, log.getCreatedAt());
+        long currentTimeMillis = System.currentTimeMillis();
+
+        redisTemplate.opsForZSet().add(KEY_TAG + log.getTag(), jsonValue, currentTimeMillis);
+
+        // TTL 3시간으로 설정
+        redisTemplate.expire(KEY_TAG + log.getTag(), TIMES, TimeUnit.SECONDS);
     }
 
 }
