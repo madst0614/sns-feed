3dc640695cfc67ed0bb5687f142a4dfcdb6e8687
HyunsooZo <bzhs1992@icloud.com>
2023-10-27 16:34:44 +0900

    feat: BaseEntity 사용을 위한 EnableJpaAuditing 어노테이션 추가

--- src/main/java/wanted/n/NApplication.java
+++ src/main/java/wanted/n/NApplication.java
@@ -2,12 +2,14 @@ package wanted.n;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class NApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NApplication.class, args);
+    }
 
 }
