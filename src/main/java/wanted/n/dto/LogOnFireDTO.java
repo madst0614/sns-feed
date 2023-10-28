d4ae328a718b0a2e4192310c25ebde3856ee1b62
12hyeon <hyeonjeong1212@naver.com>
2023-10-29 00:05:14 +0900

    feat: PriorityQueue에 따른 dto 추가

---
+++ src/main/java/wanted/n/dto/LogOnFireDTO.java
@@ -0,0 +1,16 @@
+package wanted.n.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * OnFire 후보 데이터
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public class LogOnFireDTO {
+    private long postingId;
+    private long preView;
+}
