ff5363d50f86ab10908e90b5ccf7d37c39cd6e02
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-10-28 23:46:55 +0900

    merge: 사용자 이메일 인증 추가  (#11)
    * feat: 회원가입 Request Dto를 사용해 User를 생성하는 메서드 추가
    
    엔티티 내부에 from 메서드를 사용하여 Request Dto -> User 를 반환하는
    메서드 추가
    
    * feat: User 관련 Exception 추가
    
    * chore: 이메일 전송기능을 위한 Java mail sender 의존성 추가
    
    * feat: 이메일 전송기능 추가
    
    회원가입(인증코드) 및 비밀번호 초기화에 사용 될
    SMTP 기능 추가
    
    * feat: 사용자 회원가입 기능 추가
    
    사용자 회원가입에 사용할 API 를 추가.
    - 회원가입 프로세스
      1.계정 중복 확인 (레포지토리 조회)
      2.Email 형식 확인(@Valid)
      3.비밀번호 검증은 길이(@Valid) 포함 총 5개를 진행.
      4.1~3 과정 통과시 비밀번호 암호화 하여 User 생성
      5.Role은 유저, Status는 미인증 상태로 생성
      6.입력된 이메일로 인증메일 전송
    
    * feat: 이메일 전송관련 예외메세지 추가
    
    * test: User 회원가입 테스트 코드 작성
    
    성공케이스
    실패케이스(중복된 계정,
            같은문자 3번반복,
            연속문자 3번반복,
            통상적인 비밀번호,
            영문/숫자/특문 2개이상포함)
    
    * feat: BaseEntity 사용을 위한 EnableJpaAuditing 어노테이션 추가
    
    * feat: 비동기 처리를 위한 설정 및 비동기 예외 처리 추가
    
    이메일 서비스의 응답시간 단축, 스레드 효율성증대 ,
    메일서버의 상태에 구애받지 않는 시스템 안정성을 위해
    비동기 처리 설정완료.
    + 다른 비동기처리가 필요한 서비스로 확장 가능
    
    * feat: 이메일 전송 로직 일부수정 및 비동기 처리
    
    이메일 전송 시 불필요한 로직을 최소화 하고 로깅을 통해 유지보수성 증대
    
    * feat: 회원가입 시 OTP 전송 후 redis에 해당 OTP저장하는 기능 추가
    
    전송된 OTP를 레디스에 저장 (key[email]:value[otp]) 하여
    이메일 인증 서비스를 위한 전제 기능 추가
    
    * feat: 회원 인증 관련 예외 추가
    
    * feat: 회원 이메일 인증 기능 추가
    
    이메일로 전송된 코드, 이메일, 비밀번호를 입력하면
    a. redis에 저장된 otp와 일치하는 지 확인
    b. 이메일이 가입된 이메일인지 확인
    c. 사용자의 상태를 확인 (인증/미인증)
    d. 사용자의 비밀번호 일치여부를 확인
    e. a~d까지의 단계가 모두 완료되면 사용자의 상태 업데이트
    
    * test: 회원 이메일 인증 테스트코드 추가
    
    * feat: 회원 이메일 인증 코드(OTP) 재발급 기능 추가
    
    OTP 인증 만료 시간 (10분)이 지난 경우 재발급 할 수 있도록 api 추가
    
    * comment: 사용자 인증 method 주석 추가

--- src/main/java/wanted/n/controller/UserController.java
+++ src/main/java/wanted/n/controller/UserController.java
@@ -4,11 +4,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import wanted.n.dto.UserOtpReIssueRequest;
 import wanted.n.dto.UserSignUpRequest;
+import wanted.n.dto.UserVerificationRequest;
 import wanted.n.service.EmailService;
 import wanted.n.service.RedisService;
 import wanted.n.service.UserService;
@@ -17,6 +16,7 @@ import javax.validation.Valid;
 import java.util.concurrent.CompletableFuture;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static wanted.n.enums.MailComponents.VERIFICATION_MESSAGE;
 import static wanted.n.enums.MailComponents.VERIFICATION_SUBJECT;
 
@@ -32,7 +32,8 @@ public class UserController {
 
     @PostMapping("/sign-up")
     @ApiOperation(value = "회원가입", notes = "사용자가 회원정보를 입력하여 회원가입을 진행합니다.")
-    public ResponseEntity<Void> registerUser(@Valid @RequestBody UserSignUpRequest userSignUpRequest) {
+    public ResponseEntity<Void> registerUser(
+            @Valid @RequestBody UserSignUpRequest userSignUpRequest) {
 
         // 입력받은 정보로 회원가입 (회원상태 : 미인증)
         userService.registerUser(userSignUpRequest);
@@ -48,4 +49,36 @@ public class UserController {
         return ResponseEntity.status(CREATED).build();
     }
 
+    @PatchMapping("/verification")
+    @ApiOperation(value = "회원가입 인증", notes = "회원가입 인증을 진행합니다.")
+    public ResponseEntity<Void> verifyUser(
+            @Valid @RequestBody UserVerificationRequest verificationRequest) {
+
+        // 이메일(key)로 검색된 OTP와 입력받은 OTP가 일치하는지 확인
+        redisService.otpVerification(verificationRequest.getEmail(), verificationRequest.getOtp());
+
+        // 비밀번호도 검증 하고 회원상태를 인증으로 변경
+        userService.verifyUser(verificationRequest);
+
+        return ResponseEntity.status(NO_CONTENT).build();
+    }
+
+    @PostMapping("/verification/otp-reissue")
+    @ApiOperation(value = "OTP 재발급", notes = "회원가입 인증을 위한 OTP를 재발급합니다.")
+    public ResponseEntity<Void> reissueOtp(
+            @Valid @RequestBody UserOtpReIssueRequest otpReIssueRequest) {
+
+        // 회원가입 한 사용자 이고 이메일 인증 대기 사용자 인지 확인
+        userService.checkUser(otpReIssueRequest.getEmail());
+
+        // 회원가입 인원에게 인증메일 재전송
+        CompletableFuture<String> codeCompletableFuture = emailService.sendEmail(
+                otpReIssueRequest.getEmail(), VERIFICATION_SUBJECT, VERIFICATION_MESSAGE
+        );
+
+        // 이메일(key) , 인증코드(value) 로 하여 redis 에 저장
+        redisService.saveOtp(otpReIssueRequest.getEmail(), codeCompletableFuture);
+
+        return ResponseEntity.status(CREATED).build();
+    }
 }
--- src/main/java/wanted/n/exception/ErrorCode.java
+++ src/main/java/wanted/n/exception/ErrorCode.java
@@ -20,6 +20,10 @@ public enum ErrorCode {
     INVALID_PASSWORD_SAME_CHARACTERS(HttpStatus.BAD_REQUEST, "동일한 문자가 3개 이상 연속되면 사용할 수 없습니다"),
     INVALID_PASSWORD_CONSECUTIVE_CHARACTERS(HttpStatus.BAD_REQUEST, "연속된 문자가 3개 이상 포함되면 사용할 수 없습니다"),
     INVALID_PASSWORD_USUAL_PASSWORD(HttpStatus.BAD_REQUEST, "통상적으로 자주 사용되는 비밀번호는 사용할 수 없습니다."),
+    ALREADY_VERIFIED_USER(HttpStatus.BAD_REQUEST, "이미 인증된 사용자입니다."),
+    PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    INVALID_OTP(HttpStatus.BAD_REQUEST, "인증코드가 일치하지 않습니다."),
+    OTP_EXPIRED(HttpStatus.BAD_REQUEST, "요청되지 않은 이메일이거나 인증기간이 만료된 인증번호입니다. 인증번호를 다시 요청해주세요.");
 
     // IOException
     JSON_EXCEPTION(HttpStatus.BAD_REQUEST, "Json 직렬화에 실패했습니다.");
