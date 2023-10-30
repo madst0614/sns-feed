955fdbce8805ce77bf51c9e43015b4f675bf39d9
madst0614 <81747963+madst0614@users.noreply.github.com>
2023-10-31 01:01:24 +0900

    Revert "merge: 조건 검색, Service 단위테스트 (#29)" (#31)
    This reverts commit f3be1f86e156c13d18c14d0466765622f39d34ca.
    
    Co-authored-by: junkyu lee <86291408+junkyu92@users.noreply.github.com>

--- src/main/java/wanted/n/controller/PostingController.java
+++ src/main/java/wanted/n/controller/PostingController.java
@@ -36,12 +36,13 @@ public class PostingController {
     public ResponseEntity<PostingSearchResponseDTO>  getPostingListSearchByCondition(
             @RequestHeader(AUTHORIZATION) String token,
             @RequestParam(value="hashtagname") String hashtagname
-            , @RequestParam("type")PostingType type
-            , @RequestParam("searchtype")SearchType searchType, @RequestParam("keyword") String searchKeyword
+            , @Valid @RequestParam("type")PostingType type
+            , @Valid @RequestParam("searchtype")SearchType searchType, @RequestParam("keyword") String searchKeyword
             , @PageableDefault(page=0, size = 10, sort = "createdAt", direction = Sort.Direction.ASC)Pageable pageable
     ) {
         if(hashtagname==null){
-            hashtagname = jwtTokenProvider.getAccountFromToken(token);
+            //!!warn!! getAccountFromToken 구현 필요
+//            hashtagname = jwtTokenProvider.getAccountFromToken(token);
         }
 
         return new ResponseEntity<>(postingService.getPostingList(PostingSearchRequestDTO.builder()
