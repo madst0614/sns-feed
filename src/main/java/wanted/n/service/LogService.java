ccfd4aeab3860a535c4faeca0bab63c9bc1ba064
HyeonJeong <67951802+12hyeon@users.noreply.github.com>
2023-10-31 16:40:12 +0900

    merge: 데이터 구조 및 posting url과 연결 (#34)
    * Revert "merge: 프로젝트 배포"
    
    * Revert "Revert "merge: 프로젝트 배포""
    
    * Create webhooktest.md
    
    * Update webhooktest.md
    
    * Update webhooktest.md
    
    * Update ErrorCode.java
    
    * Update UserService.java
    
    * Delete webhooktest.md
    
    * merge: 조건 검색, Service 단위테스트 (#29)
    
    * feat: 엔티티(Posting, PostingTag, Tag) 및 필드 enum(PostingType) 추가
    
    * chore: Querydsl 관련 Config 추가
    
    * chore: Search 관련 Enums 추가
    
    * refactor: Entity Class Naming 및 DB 변경에 따라 이름 수정
    
    * add: Request 및 Search 기능 관련 Dto 추가
    
    * add: PostingRepository - SearchByCondition 추가
    
    예외 고려 및 테스트 필요
    기타 기능 및 기타 Repository 추가 필요
    
    * comment: SearchByCondition 주석 수정
    
    * comment: SearchByCondition order 부분 수정
    
    * add:HashTagRepository
    
    * add: search by condition
    
    * chore: PostingType 순서 변경
    
    * add: 테스트 관련 PostingHashTagRepository 추가
    
    * add: pagination, posting errorcode
    
    * add: findhashidbyname 추가
    
    * chore: setter 삭제
    
    * refactor: searchbycondition ->findPostingPageByCondition
    
    * refactor: searchbycondition ->findPostingPageByCondition
    
    * add: 테스트 관련 PostingHashTagRepository 추가
    
    * chore: 기타 코드 수정
    
    * add: PostingService and Impl
    
    예외 고려 및 테스팅 필요
    
    * chore: 가독성 향상
    
    * chore: 기타 주석 및 코드 수정
    
    * chore: 좋아요 공유 기능 return 값 수정
    
    * refactor: 필드 수정 및 @Autowired->RequiredArgsConstructor
    
    * add : PostingServiceImpl test
    
    * delete: file
    
    * add: PostingController
    
    * chore: 빋드 설정
    
    * chore : pull 중 수정
    
    * rename: 함수 이름 수정
    
    * add: of 함수 추가
    request->condition
    
    * add: PostingController
    
    * add: Posting Controller 인증
    
    * refactor: Dto->DTO 수정
    
    * add: Service @Transactional 추가
    
    * refactor: request / response 관련 dto 추가
    
    * test: PostingService 테스트 추가
    
    * refactor: posting cnt 컬럼들 nullable 추가 및 기타 리팩토링
    
    * refactor: getPostingDetail -> upPostingViewCount
    
    upPostingViewCount만으로 getPostingDetail 기능을 수행할 수 있고
    id를 이용해서 posting만 가져오는 경우는 JPA의 findbyId로 대체 가능
    
    * bug: 의존성 주입 문제 및 뷰카운트 증가 안하던 버그 수정
    
    * refactor: null 조건 제거 및 NULL ENUM 추가
    
    * fix: Service Repos 주입 못받던 버그 수정
    
    * remove
    
    * feat: hashtabname이 null일시 토큰 계정 정보 받아옴
    
    * feat: redis list 및 doFilter의 url 수정
    
    * feat: Scheduler 수정
    
    * merge: other branch
    
    ---------
    
    Co-authored-by: HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
    Co-authored-by: junkyu lee <86291408+junkyu92@users.noreply.github.com>
    Co-authored-by: madst0614 <81747963+madst0614@users.noreply.github.com>

--- src/main/java/wanted/n/service/LogService.java
+++ src/main/java/wanted/n/service/LogService.java
@@ -9,7 +9,6 @@ import wanted.n.dto.LogOnFireDTO;
 import wanted.n.exception.ErrorCode;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static wanted.n.enums.MailComponents.*;
 
@@ -24,44 +23,22 @@ public class LogService {
     private final static String KEY_HOT_HASHTAG = "tags"; // 핫 해시태그 리스트를 저장하는 키
     private final static String KEY_POSTING = "posting:"; // 리스트를 저장하는 키
 
-    private final static Integer MAX_TAG = 5;
     private final static Integer MAX_POSTING = 10;
-
 
     /**
      * 최근 3시간 동안 많이 사용된 해시태그 리스트를 조회
      * 조건 1) 최대 5개 = MAX_TAG
      */
+
+    // 최근 3시간 동안 많이 사용된 해시태그 저장
     public HashtagsResponseDTO getSortedTags() {
-        List<Object> hashtags = new ArrayList<>(redisService.findHotTags(MAX_TAG));
+        List<Object> hashtags = new ArrayList<>(redisService.findHotTags());
         return new HashtagsResponseDTO(ErrorCode.HOT_HASHTAG_OK, hashtags);
     }
 
-    // 최근 3시간 많이 사용된 태그 순으로 리스트 저장
+    // 많이 사용된 해시태그 데이터 조회
     public void getCountByTagForLast3Hours() {
-        Set<String> tags = redisService.findSetDataWithKey(KEY_TAG + "*");
-
-        List<String> sortedTags = countTags(tags);
-        redisService.saveSortedTags(sortedTags, KEY_HOT_HASHTAG);
-    }
-
-    // 태그마다 시간 범위 내의 개수를 세어서 내림차순으로 정렬된 결과 반환
-    private List<String> countTags(Set<String> tags) {
-        Map<String, Long> tagCounts = new HashMap<>();
-
-        for (String tag : tags) {
-            long count = redisService.countDataWithTime(tag);
-            tagCounts.put(tag, count); // .substring(KEY_TAG.length() ?
-        }
-        return sortTagsByCount(tagCounts);
-    }
-
-    // 내림차순으로 태그 Map 정렬
-    private List<String> sortTagsByCount(Map<String, Long> tagCounts) {
-        return tagCounts.entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
+        redisService.saveSortedTags();
     }
 
 
